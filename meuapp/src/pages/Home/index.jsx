94172cbbabb1ee5e99a722d9935de65c8dc6a920
Antonio Fernando <jufer376@gmail.com>
2025-06-02 09:30:37 -0300

    Adicionando estilos e reponsividade
    Na aula número 30 o professor utilizou métodos para fazer requisições e mostrar na página.

--- meuapp/src/pages/Home/index.jsx
+++ meuapp/src/pages/Home/index.jsx
@@ -1,48 +1,42 @@
-// Importa os hooks useEffect e useState do React
-import { useEffect, useState } from "react";
-
-// Importa o módulo de requisições HTTP (axios configurado) do arquivo api.js
+import React, { useEffect, useState } from "react";
 import api from "../../services/api";
+import {Link} from 'react-router-dom';
+import './Home.css'
 
 function Home() {
-    // Cria um estado chamado 'filmes' com valor inicial como array vazio
-    // A função 'setfilmes' será usada para atualizar esse estado
     const [filmes, setfilmes] = useState([]);
 
-    // useEffect será executado apenas uma vez quando o componente for montado (porque o array de dependências está vazio)
     useEffect(() => {
-        // Função assíncrona para carregar os filmes da API
         async function loadFilmes() {
-            // Faz uma requisição GET para o endpoint "movie/now_playing"
             const response = await api.get("movie/now_playing", {
                 params: {
-                    api_key: "f473c8c0ce90b372a47df5442d56c3ec", // Chave da API TMDB
-                    language: "pt-BR", // Define o idioma da resposta como português brasileiro
-                    page: 1 // Pega a primeira página dos resultados
+                    api_key: "f473c8c0ce90b372a47df5442d56c3ec",
+                    language: "pt-BR",
+                    page: 1
                 }
             });
 
-            // Exibe os dados retornados da API no console do navegador
-            console.log(response.data);
-
-            // (Melhoria possível): Atualizar o estado com os filmes retornados
-            setfilmes(response.data.results);
+            console.log(response.data.results.slice(0, 10));
+            setfilmes(response.data.results.slice(0, 10));
         }
 
-        // Chama a função que carrega os filmes
         loadFilmes();
     }, []);
 
-    // Renderiza o conteúdo da página
     return (
-        <div>
-            
-            <ul>
-                {filmes.map(filme => (
-                    <li key={filme.id}>{filme.title}</li>
-                ))}
-            </ul>
-            
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme) => {
+                    return (
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+ alt={filme.title}/>           
+                            <Link to={`/filme/${filme.id}`} >Acessar</Link>
+                        </article>
+                    );
+                })}
+            </div>
         </div>
     );
 }
