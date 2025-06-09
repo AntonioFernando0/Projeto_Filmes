1b61373585e6d5eb7e786320abb9b74aecc09e3f
Antonio Fernando <jufer376@gmail.com>
2025-06-09 16:35:16 -0300

    Adicionando funcionalidades
    Foi criado novas funcionalidades no projeto filmes.

--- meuapp/src/pages/Filme/index.jsx
+++ meuapp/src/pages/Filme/index.jsx
@@ -1,13 +1,62 @@
-import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react'; 
+import { useParams } from 'react-router-dom'; // Hook para pegar parâmetros da URL
+import { useEffect, useState } from 'react';  // Hooks do React
+import './filme.css'
+import api from '../../services/api';         // Configuração do axios
 
 function Filme() {
-  const { id } = useParams();
+  const { id } = useParams(); // Pega o parâmetro "id" da URL (ex: /filme/123)
+  
+  const [filme, setFilmes] = useState({});    // Armazena os dados do filme
+  const [loading, setLoading] = useState(true); // Estado para mostrar "carregando"
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api.get(`/movie/${id}`, {
+        params: {
+          api_key: "f473c8c0ce90b372a47df5442d56c3ec", // Chave da API do TMDB
+          language: "pt-BR"                            // Idioma da resposta
+        }
+      })
+      .then((response) => {
+        setFilmes(response.data); // Salva os dados do filme
+        setLoading(false);        // Remove o "carregando"
+      })
+      .catch(() => {
+        console.log("Erro ao buscar o filme."); // Erro no fetch
+        setLoading(false); // Mesmo com erro, remove o "carregando"
+      });
+    }
+
+    loadFilme(); // Chamada da função dentro do useEffect
+  }, [id]); // Executa sempre que o ID mudar
+
+  if (loading) {
+    return (
+      <div className='filme-info'>
+        <h1>Carregando detalhes</h1>
+      </div>
+    );
+  }
 
   return (
-    <div>
-      <h1>Bem-vindo aos Filmes!</h1>
-      <p>Você está vendo o filme com ID: {id}</p>
+    <div className='filme-info'>
+      <h1>{filme.title}</h1> {/* Título do filme */}
+      <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+      <span className='sino'>Sinopse</span>
+      <p> {filme.overview}</p> {/* Sinopse */}
+      <strong>Nota: {filme.vote_average.toFixed(1)}/10</strong> {/* Nota média */}
+
+      <div className='area-button'>
+          <button>
+            Salvar
+          </button>
+          <button>
+            <a href='#'>
+              Trailer
+            </a>
+          </button>
+
+      </div>
     </div>
   );
 }
